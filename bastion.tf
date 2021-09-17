88d15be77ba4c94558f9101f092ad8de48a207cd
tameek <tameek@gmail.com>
2021-09-17 16:35:31 -0400

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -66,3 +66,11 @@ resource "azurerm_virtual_machine" "avm" {
     Environment = "lab"
     }
 }
+
+
+#################################OUTPUTS#################################
+
+data "azurerm_public_ip" "bastionpubip" {
+  name                = reverse(split("/", tolist(azurerm_kubernetes_cluster.aks.network_profile.0.load_balancer_profile.0.effective_outbound_ips)[0]))[0]
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+}
